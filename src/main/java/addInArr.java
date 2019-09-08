ecc0dc7e74224309004991b44c0080e7db648d6b
phongtiger <phongken94@gmail.com>
2019-09-08 21:01:30 +0700

    tailen

--- src/main/java/addInArr.java
+++ src/main/java/addInArr.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 public class addInArr {
     public static void main(String[] args) {
-        int[] arr = new int[10];
+        int[] arr = new int[5];
         arr[0]=1;
         arr[1]=2;
         arr[2]=5;
         arr[3]=4;
         arr[4]=8;
+        int[] arr1 = new int[6];
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter X: ");
@@ -18,10 +19,18 @@ public class addInArr {
         if (index <= 1 && index > arr.length - 1) {
             System.out.println("Not add X in Arr");
         } else {
-            for (int i = index - 1; i < arr.length - 1; i++) {
-
-                arr[i + 1] = arr[i];
+            for (int i = 0; i < arr.length+1 ; i++) {
+                if (i < index) {
+                    arr1[i]= arr[i];
+                } else if (i == index) {
+                    arr1[i]= X;
+                } else {
+                    arr1[i] = arr[i-1];
+                }
             }
+        }
+        for (int element : arr1) {
+            System.out.println(element+"\n");
         }
 
     }
